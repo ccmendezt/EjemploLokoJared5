6e8ffe2e852fd0c4544264fe396c6c68bc4e37cd
ccmendezt <camilomendez1903@gmail.com>
2021-10-09 21:27:35 -0500

    Hice un proyecto en GitHub - Prueba

---
+++ src/ejemplolokojared5/EjemploLokoJared5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplolokojared5;
+
+/**
+ *
+ * @author CRISTIAN CAMILO
+ */
+public class EjemploLokoJared5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Proyecto GitHub");
+    }
+    
+}
